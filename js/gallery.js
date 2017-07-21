49e9a09e4abfdd1592640765d58e73efe33146c4
Chelsea Clay Work <chelseaclayyy@yahoo.com>
2017-07-21 07:16:15 -0400

    Updates

--- js/gallery.js
+++ js/gallery.js
@@ -15,8 +15,8 @@ var portfolio = [
     lightbox: "img/portfolio/develop/project4.jpg",
     title: "Interactive Photo Gallery",
     caption: "In this project, you will create an interactive photo gallery using JavaScript and jQuery. Thumbnails and photos will be provided with descriptions. At the top of the page you'll have a search area where photos will hide and show depending on user input. When the user clicks on a thumbnail the photo will display in a lightbox. There should be a back and previous buttons to cycle through photos.",
-    githubLink: "https://chelseaclay.github.io/Project4_InteractivePhotoGallery/",
-    liveLink: "https://github.com/chelseaclay/Project4_InteractivePhotoGallery",
+    githubLink: "https://github.com/chelseaclay/Project4_InteractivePhotoGallery",
+    liveLink: "https://chelseaclay.github.io/Project4_InteractivePhotoGallery/",
     skills: ["HTML,", "CSS,", "Git,", "JavaSctipt,", "Jquery"],
     kind: "Develop"
   },
